954e110540a6da15a0700d425e2a0969ef8eb8de
RAIS AKBAR SIDIK <rais.akbar3110@gmail.com>
2023-06-16 15:18:48 +0700

    add and instal  boostrap

---
+++ src/pages/Article.jsx
@@ -0,0 +1,11 @@
+function Article() {
+
+    return (
+      <>
+        <h1>Article</h1>
+      </>
+    )
+  }
+  
+  export default Article
+  
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../component/navbar"
+
+function Home() {
+
+    return (
+      <>
+        <h1>Home</h1>
+        <Navbar/>
+      </>
+    )
+  }
+  
+  export default Home
+  
